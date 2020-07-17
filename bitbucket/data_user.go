1a5a9763e08ce195435a7a981abcb7b9ee028d7a
Martin Litvaj <kamahl19@gmail.com>
2020-07-17 16:00:46 +0200

    Implement data source bitbucketserver_user

---
+++ bitbucket/data_user.go
@@ -0,0 +1,35 @@
+package bitbucket
+
+import (
+	"github.com/hashicorp/terraform/helper/schema"
+)
+
+func dataSourceUser() *schema.Resource {
+	return &schema.Resource{
+		Read: dataSourceUserRead,
+
+		Schema: map[string]*schema.Schema{
+			"name": {
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"email_address": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"display_name": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"user_id": {
+				Type:     schema.TypeInt,
+				Computed: true,
+			},
+		},
+	}
+}
+
+func dataSourceUserRead(d *schema.ResourceData, m interface{}) error {
+	d.SetId(d.Get("name").(string))
+	return resourceUserRead(d, m)
+}
---
+++ bitbucket/data_user_test.go
@@ -0,0 +1,31 @@
+package bitbucket
+
+import (
+	"testing"
+
+	"github.com/hashicorp/terraform/helper/resource"
+)
+
+func TestAccBitbucketDataUser(t *testing.T) {
+	config := `
+		data "bitbucketserver_user" "test" {
+			name = "admin"
+		}
+	`
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:  func() { testAccPreCheck(t) },
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: config,
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("data.bitbucketserver_user.test", "name", "admin"),
+					resource.TestCheckResourceAttr("data.bitbucketserver_user.test", "email_address", "admin@example.com"),
+					resource.TestCheckResourceAttr("data.bitbucketserver_user.test", "display_name", "Admin"),
+					resource.TestCheckResourceAttr("data.bitbucketserver_user.test", "user_id", "1"),
+				),
+			},
+		},
+	})
+}
