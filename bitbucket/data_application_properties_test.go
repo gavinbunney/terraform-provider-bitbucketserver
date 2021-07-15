f0730e2f649c66763d36c44baed1adeb33fbe2e6
Gavin Bunney <409207+gavinbunney@users.noreply.github.com>
2021-07-15 10:02:41 -0700

    Test fixes

--- bitbucket/data_application_properties_test.go
+++ bitbucket/data_application_properties_test.go
@@ -19,8 +19,8 @@ func TestAccBitbucketDataApplicationProperties(t *testing.T) {
 				Config: config,
 				Check: resource.ComposeTestCheckFunc(
 					resource.TestCheckResourceAttr("data.bitbucketserver_application_properties.main", "version", "6.10.0"),
-					resource.TestCheckResourceAttr("data.bitbucketserver_application_properties.main", "build_number", "6007000"),
-					resource.TestCheckResourceAttr("data.bitbucketserver_application_properties.main", "build_date", "1569809627115"),
+					resource.TestCheckResourceAttr("data.bitbucketserver_application_properties.main", "build_number", "6010000"),
+					resource.TestCheckResourceAttr("data.bitbucketserver_application_properties.main", "build_date", "1578626301641"),
 					resource.TestCheckResourceAttr("data.bitbucketserver_application_properties.main", "display_name", "Bitbucket"),
 				),
 			},
