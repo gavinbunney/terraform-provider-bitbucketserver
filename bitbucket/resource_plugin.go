4e1cd58c9b91055b559dcdeb2e412b4e871390b2
Gavin Bunney <409207+gavinbunney@users.noreply.github.com>
2021-07-15 08:45:51 -0700

    fmt

--- bitbucket/resource_plugin.go
+++ bitbucket/resource_plugin.go
@@ -296,7 +296,7 @@ func resourcePluginCreate(d *schema.ResourceData, m interface{}) error {
 
 	d.SetId(key)
 
-	err = resource.Retry(time.Minute * 2,
+	err = resource.Retry(time.Minute*2,
 		func() *resource.RetryError {
 			exists, err := resourcePluginExists(d, m)
 			if exists == false || err != nil {
@@ -310,7 +310,7 @@ func resourcePluginCreate(d *schema.ResourceData, m interface{}) error {
 	}
 
 	// need to also run an update loop to set enabled flags and license details
-	err = resource.Retry(time.Minute * 2,
+	err = resource.Retry(time.Minute*2,
 		func() *resource.RetryError {
 			err := resourcePluginUpdate(d, m)
 			if err != nil {
